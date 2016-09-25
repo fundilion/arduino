897f8bd16fff7273310393585572e1410b20de67
Guillermo Laoretani <guillermo.laoretani@jivesoftware.com>
2016-09-25 00:48:22 -0300

    Added JavArduino file

---
+++ JavArduino.ino
@@ -0,0 +1,34 @@
+#include <AFMotor.h>
+
+AF_Stepper motor(200,1);
+
+char captura;
+void setup (){
+
+  motor.setSpeed(150);
+  Serial.begin(9600);
+
+}
+void loop (){
+ 
+ captura = Serial.read();
+    
+  switch(captura){
+    case '3':
+      motor.step (3,BACKWARD,MICROSTEP);
+      break;
+    case '4':
+     motor.step (3,FORWARD,MICROSTEP);
+      break;
+    case '7':
+       motor.step(200,BACKWARD,MICROSTEP);
+      break;
+    case '8':
+     motor.step(200,FORWARD,MICROSTEP);
+      break;
+  }
+}
+ 
+  
+
+  
